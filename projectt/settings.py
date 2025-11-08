ba2c05d09d2e737ca3f80e7a1d2e0ca5e8dbb988
unknown <amp2052@gmail.com>
2025-11-08 15:51:38 +0530

    latest add

--- projectt/settings.py
+++ projectt/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security
 # ---------------------------
 SECRET_KEY = "django-insecure-5&w=n(6wlenqm5_x)c6fv2qu#v(h)+rp5wx3bujml&a3olpt_+"
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["*"]  # replace with your domain in production
 
 # ---------------------------
