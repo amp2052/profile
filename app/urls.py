a3e01ec370c3fd7bed3ce722e03fabd9cfa284be
amp2052 <amp2052@gmail.com>
2026-04-22 23:33:00 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,4 @@
-from django.contrib.sitemaps.views import sitemap
-from app.sitemaps import StaticViewSitemap
-
-sitemaps = {
-    'static': StaticViewSitemap,
-}
-
-urlpatterns = [
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
-]from django.urls import path
+from django.urls import path
 from . import views
 
 urlpatterns = [
